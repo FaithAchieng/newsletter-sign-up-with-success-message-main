0b45a82cf5964257bc6eb712976078b37b2fe949
FaithAchieng <faithachienglexus@gmail.com>
2024-11-18 17:48:41 +0300

    added for small screens

--- script.js
+++ script.js
@@ -1,43 +1,77 @@
-document.addEventListener('DOMContentLoaded', function() {
+const email=document.getElementById('email');
+const form=document.getElementById('form');
+const error=document.getElementById('error');
+const submit=document.getElementById('submit');
+const updates=document.getElementById('updates');
+const subscribe=document.getElementById('subscribe');
+const dismiss=document.getElementById('dismiss');
+submit.addEventListener('click',(e) => {
+    let messages=[];
 
-    const  UpdateForm = document.querySelectorAll('.update.hide');
-    const  SuccessForm = document.querySelectorAll('.subscribe');
-    const SubscribeButton = document.getElementById('sub');
-    const DismissButton = document.getElementById('dismiss');
-    const InputField = document.getElementById('email');
-
-    InputField.addEventListener('input', function(value){
-
-        const EmailInput = this.value;
-        const EmailArray = EmailInput.split(/[@]/);
-        console.log(EmailArray);
-        console.log(EmailArray.length);
-
-    })
-
-
-    SubscribeButton.addEventListener('click', (e)=>{
+    if(email.value==="ash#loremcompany.com" || email.value===""){
+        messages.push('Enter valid email address');
+        email.style.backgroundColor='red';
+    }
+    if(messages.length>0){
         e.preventDefault();
-        function toggleUpdateForm (){
-            UpdateForm.forEach(form => form.style.display = 'none');
-        }
-
-        if(UpdateForm && EmailArray.length!= 0){
-            toggleUpdateForm();
-
-            SuccessForm[0].style.display = 'block';
-            console.log(UpdateForm)
-        }
-        
-    });
-  
-    DismissButton.addEventListener('click',  (e)=>{
-     e.preventDefault();
-     function showUpdateForm(){
-        UpdateForm.forEach(form => form.style.display = 'block');
-     }
-     showUpdateForm();
-     SuccessForm[0].style.display = 'none';
-
-    })
+        error.innerText=messages.join(' ,');
+    }
+    if(messages.length==0){
+        e.preventDefault();
+        updates.style.display="none";
+        subscribe.style.display="block";
+    }
 });
+dismiss.addEventListener("click", (e)=>{
+        e.preventDefault();
+        updates.style.display="block";
+        subscribe.style.display="none";
+        error.innerText='';
+    
+});
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     const  UpdateForm = document.querySelectorAll('.update.hide');
+//     const  SuccessForm = document.querySelectorAll('.subscribe');
+//     const SubscribeButton = document.getElementById('sub');
+//     const DismissButton = document.getElementById('dismiss');
+//     const InputField = document.getElementById('email');
+
+//     InputField.addEventListener('input', function(value){
+
+//         const EmailInput = this.value;
+//         const EmailArray = EmailInput.split(/[@]/);
+//         console.log(EmailArray);
+//         console.log(EmailArray.length);
+
+//     })
+
+
+//     SubscribeButton.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         function toggleUpdateForm (){
+//             UpdateForm.forEach(form => form.style.display = 'none');
+//         }
+
+//         if(UpdateForm && EmailArray.length!= 0){
+//             toggleUpdateForm();
+
+//             SuccessForm[0].style.display = 'block';
+//             console.log(UpdateForm)
+//         }
+        
+//     });
+  
+//     DismissButton.addEventListener('click',  (e)=>{
+//      e.preventDefault();
+//      function showUpdateForm(){
+//         UpdateForm.forEach(form => form.style.display = 'block');
+//      }
+//      showUpdateForm();
+//      SuccessForm[0].style.display = 'none';
+
+//     })
+// });
